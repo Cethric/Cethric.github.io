47d321121ab25bdaca3f38215655304f3ac88215
Cethric <roganBlake@gmail.com>
2015-03-21 12:55:44 +1100

    Fixed up css and java issues

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,10 @@
 function projectExpander() {
     var expander = document.getElementById("menubarProj")
-    var children = expander.children;
-    for (var childID in children) {
-        var child = expander.children[childID];
-        if (child != "undefined") {
-            if (child.style.display == "none") {
-                child.style.display = "block";
-                document.getElementById("projExpander").innerHTML = "-"
-            } else {
-                child.style.display = "none";
-                document.getElementById("projExpander").innerHTML = "+"
-            }
-        }
+    if (expander.style.display == "none") {
+        expander.style.display = "block"
+        document.getElementById("projExpander").innerHTML = "-"
+    } else {
+        expander.style.display = "none"
+        document.getElementById("projExpander").innerHTML = "+"
     }
 }
