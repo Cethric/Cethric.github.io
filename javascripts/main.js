7b47bf16743a3ceccb37458eef65cd74b217d440
Cethric <roganBlake@gmail.com>
2015-03-21 12:45:40 +1100

    Updated website a little. More to come

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+function projectExpander() {
+    var expander = document.getElementById("menubarProj")
+    var children = expander.children;
+    for (var childID in children) {
+        var child = expander.children[childID];
+        if (child != "undefined") {
+            if (child.style.display == "none") {
+                child.style.display = "block";
+                document.getElementById("projExpander").innerHTML = "-"
+            } else {
+                child.style.display = "none";
+                document.getElementById("projExpander").innerHTML = "+"
+            }
+        }
+    }
+}
