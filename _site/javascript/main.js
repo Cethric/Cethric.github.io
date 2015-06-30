ded9b305d69b6a231cd4024c448da9128eacd342
Cethric <roganBlake@gmail.com>
2015-06-30 14:22:00 +1000

    Automated Update

--- _site/javascript/main.js
+++ _site/javascript/main.js
@@ -1,19 +1,21 @@
-var list = document.getElementById("project_list");
-var posts = document.getElementById('posts');
+function menu_setup() {
+    var list = document.getElementById("project_list");
+    var posts = document.getElementById('posts');
 
-list.onclick = function(e) {
-    e.stopPropagation();
-    if (list.style.display=='none') {
-        list.style.display = 'block';
-    } else {
-        list.style.display = 'none';
+    list.onclick = function(e) {
+        e.stopPropagation();
+        if (list.style.display=='none') {
+            list.style.display = 'block';
+        } else {
+            list.style.display = 'none';
+        }
     }
-}
 
-function more_menu() {
-    if (posts.style.right=='-50em') {
-        posts.style.right = "0";
-    } else {
-        posts.style.right = "-50em";
+    function more_menu() {
+        if (posts.style.right=='-50em') {
+            posts.style.right = "0";
+        } else {
+            posts.style.right = "-50em";
+        }
     }
 }
